7d2fb35624a85250e1d7a70a17628c9833222367
Carlos Mogort <carlosmogortbrines@gmail.com>
2024-05-07 11:21:44 +0200

    first commit

--- src/Main2.java
+++ src/Main2.java
@@ -1,5 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        String frase = "";
+        try{
+            fw = new FileWriter("hola.txt");
+            bw = new BufferedWriter(fw);
+            do {
+                frase = Leer.leerTexto("Di la frase: ");
+                    if (!frase.equalsIgnoreCase("fin")) {
+                        bw.write(frase);
+                        bw.newLine();
+                }
+            }while (!frase.equalsIgnoreCase("Fin"));
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
