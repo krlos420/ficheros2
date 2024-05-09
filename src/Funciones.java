7552023acb311505a74b8a7c4fba1eab1b697199
Carlos Mogort <carlosmogortbrines@gmail.com>
2024-05-09 12:13:21 +0200

    casi commit 2

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,17 +1,8 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
+import java.io.RandomAccessFile;
 public class Funciones {
     public static void generarFicheroEnteros(File f, int cantidad) throws IOException {
-        Random numRandom = new Random();
-        int num;
-        try {
-            for (int i = 0; i < cantidad; i++) {
-                num = numRandom.nextInt();
 
-            }
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
     }
 }
